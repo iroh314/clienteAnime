40489e2c163c6acbbec51e3666af852e9b96f3b6
Jhon Sanchez <iroh314@gmail.com>
2021-06-22 22:15:37 -0500

    Se realiza las configuraciones iniciales para consumir los datos del API

--- controllers/conexion.js
+++ controllers/conexion.js
@@ -17,3 +17,45 @@ fetch(url,peticion)
 .then(function(datos){
     console.log(datos)
 })
+
+//4. Funcion para pintar los datos de llegada
+function pintarPersonajes(datos){
+
+    let fila = document.getElementById("fila");
+    //Este elemento nos permite recorrer la información del arreglo de JS
+    datos.map(function(personajes){
+
+        let columna = document.createElement("div");
+        columna.classList.add("col");
+
+        let tarjeta = document.createElement("div");
+        tarjeta.classList.add("card");
+        tarjeta.classList.add("h-100");
+
+        let imagen = document.createElement("img");
+        imagen.classList.add("card-img-top");
+        imagen.src = personajes.foto;
+
+        let tarjetaCuerpo = document.createElement("div");
+        tarjetaCuerpo.classList.add("card-body");
+
+        let nombre = document.createElement("h3");
+        nombre.classList.add("text-center");
+        nombre.textContent = personajes.nombre;
+
+        let descripcion = document.createElement("p");
+        descripcion.classList.add("text-center");
+        descripcion.textContent = personajes.descripcion;
+
+        //Se definen los hijos de los elementos previamente creados
+        tarjetaCuerpo.appendChild(nombre);
+        tarjetaCuerpo.appendChild(descripcion);
+
+        tarjeta.appendChild(imagen);
+        tarjeta.appendChild(tarjetaCuerpo);
+
+        columna.appendChild(tarjeta);
+
+        fila.appendChild(columna);
+    });
+}
